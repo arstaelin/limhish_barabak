16a6d133291f42fae839d62e21a0ca9e165c9b6e
NirMagrafta <nir.magrafta@gmail.com>
2016-02-25 19:33:03 +0200

    Create svm_with_pca_test.m

---
+++ 204805543.316030055.316553213/svm/svm_with_pca_test.m
@@ -0,0 +1,13 @@
+function [ predictions ] = svm_with_pca_test( model, data )
+%SVM_TEST returns predictions of given SVM model on data
+
+svm_model = model{1};
+words_used = model{2};
+v = model{3};
+
+data = convert_to_vec(data);
+data = data(:,words_used);
+data = data*v;
+[predictions] = svmpredict(zeros(size(data,1),1), data, svm_model, '-q');
+
+end
