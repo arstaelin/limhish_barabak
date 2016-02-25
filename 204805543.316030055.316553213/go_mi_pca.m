87a99f8859cbf278ca071f42c7d8a0bf2f7ae8af
NirMagrafta <nir.magrafta@gmail.com>
2016-02-25 19:40:05 +0200

    Update and rename go_mi_svm.m to go_mi_pca.m

--- 204805543.316030055.316553213/go_mi_pca.m
+++ 204805543.316030055.316553213/go_mi_pca.m
@@ -1,4 +1,4 @@
 function go_mi_pca (directory) 
-	load('model_adaboost.mat', 'model_mi_pca');
+	load('model_mi_pca.mat', 'model_mi_pca');
 	run_classifier( directory, model_mi_pca, @vector_basic, @svm_with_pca_test);
 end
